c12b7fecbf069e5b90c3915cad65726a92dbf46e
redoutable95 <83347740+redoutable95@users.noreply.github.com>
2022-12-03 19:26:48 +0500

    update

--- scripts/script.js
+++ scripts/script.js
@@ -22,28 +22,3 @@ document.addEventListener("DOMContentLoaded", () => {
     body.classList.toggle("active");
   });
 });
-
-const form = document.querySelector(".form");
-const inputs = form.querySelectorAll(".form__input");
-const submit = form.querySelector(".form__btn");
-const modalClose = form.querySelector(".modal-close");
-const token = "5718758078:AAH1OWJmqfFS5OqAK4Ut-wKefQCGlQDNrRw";
-const chat_id = "5684639400";
-let msg = "";
-
-submit.addEventListener("click", (e) => {
-  let stack = [];
-  e.preventDefault();
-  for (let i = 0; i < inputs.length; i++) {
-    stack[i] = inputs[i].value;
-    inputs[i].value = "";
-  }
-  msg = `Name: ${stack[0]} Phone: ${stack[1]} Email: ${stack[2]} Message: ${stack[3]}`;
-  form.classList.add("active");
-  fetch(
-    `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&parse_mode=html&text=${msg}`
-  );
-});
-modalClose.addEventListener("click", () => {
-  form.classList.remove("active");
-});
